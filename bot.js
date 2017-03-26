bd8072a1dedc35f22ab1f580ef651ad033d1bbf4
Kevin Pang <kpanghmc@gmail.com>
2017-03-26 03:27:38 -0700

    Added express in case we want a web interface

--- bot.js
+++ bot.js
@@ -1,4 +1,15 @@
-var fs = require('fs');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+  response.send('Bot is running');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 try {
 	var Discord = require("discord.js");
